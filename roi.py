9dcebcf6c316297c09cdc6ff3b2c32c3e21ebdde
GarnerEvans <gevans.1414@gmail.com>
2022-11-27 16:32:30 -0800

    second commit

--- roi.py
+++ roi.py
@@ -10,12 +10,18 @@ class property():
         self.total_expenses = None
 
     def annual_ROI(self):
-        expense_1 = int(input('Amount of first expense:'))
-        expense_2 = int(input('Amount of second expense:'))
-        expense_3 = int(input('Amount of third expense:'))
-        expense_4 = int(input('Amount of fourth expense:'))
-        expense_5 = int(input('Amount of fifth expense:'))
-        self.total_expenses = expense_1 + expense_2 + expense_3 + expense_4 + expense_5
+        extra_income = input("Does your property generate income from laundy, storage or any other miscelanious tenant expense? If yes, enter the total amount, if no enter 'no'.")
+        if extra_income == int:
+            self.rent = self.rent + extra_income
+        taxes = int(input('Enter the amount of taxes paid:'))
+        insurance = int(input('Enter your insurance payment amount:'))
+        utilities = int(input('Enter total amount of utilities paid (water, electric, gas, etc):'))
+        HOA = int(input('Enter HOA payment amount:'))
+        vacancy = int(input('Enter amount paid due to vacancy:'))
+        repairs =  int(input('Enter amount allocated for repairs:'))
+        property = int(input("Enter amount paid in property management expenses:"))
+        mortgage = int(input("Enter amount of your mortgage payment:"))
+        self.total_expenses = taxes + insurance +utilities + HOA + vacancy + repairs + property + mortgage
         print(f'Your total expenses are {self.total_expenses}')
         monthly_flow = self.rent - self.total_expenses
         print(f'Your monthly cash flow is {monthly_flow}')
@@ -27,8 +33,9 @@ class property():
         elif response == 'continue':
             down_payment = estimated_down_payment
             # return down_payment
-        other_costs = input("Please enter the total value of any other costs associated with the purchase of your property. This could be closing costs, rehabilitation budget, or other miscellanious expenses:")
-        total_investment = down_payment + int(other_costs)
+        closing_costs = int(input("Enter the amount you paid in closing costs:"))
+        rehab_budget = int(input("Enter the amount allocated for a rehabilitation budget:"))
+        total_investment = down_payment + closing_costs + rehab_budget
         annual_flow = monthly_flow * 12
         return_on_investment = (annual_flow/total_investment) * 100
         print(f"Your annual return on investment is {return_on_investment} percent")
